7f33d04956a26f6d57e1b4188a69f4d80f90b402
RHETT CHASE <rhettjchase@gmail.com>
2023-10-27 12:04:54 -0700

    solved sum problem

--- js/app.js
+++ js/app.js
@@ -8,8 +8,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) {
-  //eslint-disable-line
+function sum(a, b) { //eslint-disable-line
   let total = a + b;
   return [total, `The sum of ${a} and ${b} is ${total}.`];
 }
